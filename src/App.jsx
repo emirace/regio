6ffee5cfc9c3cbb959482e5a0b2dc04ba808209f
emirace <emmanuelakwuba57@gmail.com>
2024-07-06 13:39:12 +0100

    yolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import Button from "./components/Button";
 
 function App() {
-  const navigate = useNavigate();
   return (
     <div className="bg-primary flex flex-col gap-10 justify-center items-center h-screen">
       <div className="absolute top-5 left-10 text-4xl font-bold text-white">
         Regio
       </div>
       <FaLocationDot className="text-white animate-bounce text-6xl" />
-      <Button
-        onClick={() => navigate("/auth/register")}
-        className="bg-white text-[#013d3d] "
+      <Link
+        to={"/auth/register"}
+        className="text-lg font-medium p-2 px-4 rounded-md bg-white text-[#013d3d] "
       >
         Start now
-      </Button>
+      </Link>
+      <Link
+        to={"/map"}
+        className="text-lg font-medium p-2 px-4 rounded-md bg-white text-[#013d3d] "
+      >
+        Goto Map
+      </Link>
     </div>
   );
 }
